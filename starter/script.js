8da4b89a58ba9cdfa9ff97ae21e0c991ea52f7d9
Gustavo Marialva <gmarialva@gmail.com>
2022-10-20 09:52:00 -0300

    adiciona event click e msg de erro

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,16 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent;
+// document.querySelector('.score').textContent;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No number!';
+  }
+});
